4edaf2c54f430bbd0e68b6a003bd2d78fa5e2acb
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2022-11-21 22:43:15 -0500

    Try to prevent crash without AE2

--- src/main/java/thaumic/tinkerer/common/block/tile/TileAspectAnalyzer.java
+++ src/main/java/thaumic/tinkerer/common/block/tile/TileAspectAnalyzer.java
@@ -41,6 +41,7 @@ import java.util.Map;
 @Optional.InterfaceList({
         @Optional.Interface(iface = "li.cil.oc.api.network.SimpleComponent", modid = "OpenComputers"),
         @Optional.Interface(iface = "dan200.computercraft.api.peripheral.IPeripheral", modid = "ComputerCraft")
+        @Optional.Interface(iface = "appeng.api.movable.IMovableTile", modid = "appliedenergistics2")
 })
 public class TileAspectAnalyzer extends TileEntity implements IInventory, SimpleComponent, IPeripheral, IMovableTile {
 
@@ -244,11 +245,13 @@ public class TileAspectAnalyzer extends TileEntity implements IInventory, Simple
     }
 
     @Override
+    @Optional.Method(modid = "appliedenergistics2")
     public boolean prepareToMove() {
         return true;
     }
 
     @Override
+    @Optional.Method(modid = "appliedenergistics2")
     public void doneMoving() {
 
     }
